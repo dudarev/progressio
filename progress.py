8b858cc20c95341a779b4bec406e93ee49ec3aca
Artem Dudarev <dudarev@locograph.(none)>
2010-04-12 21:15:52 +0300

    parsing and printing of all fields, new step to do - clean

--- progress.py
+++ progress.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+import yaml
 
-for line in open("progress.txt"):
-    print line
+for i in yaml.load_all(open('progress.yaml')):
+    key = i.keys()[0]
+    done = i[key].get("done",False)
+    if not done and i[key].has_key('title'):
+        print "%s: %s" % (key,i[key]['title'])
