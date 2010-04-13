baacf9853bcfa4bed76b0f585643f0df6353f763
Artem Dudarev <dudarev@locograph.(none)>
2010-04-13 12:12:22 +0300

    accepts a command, new stesps added

--- progress.py
+++ progress.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python
 import yaml
+import sys
 
-for i in yaml.load_all(open('progress.yaml')):
-    key = i.keys()[0]
-    done = i[key].get("done",False)
-    if not done and i[key].has_key('title'):
-        print "%s: %s" % (key,i[key]['title'])
+def main():
+
+    args = sys.argv
+    command = None
+    if len(args) > 1:
+        command = args[1]
+        print command
+
+    for i in yaml.load_all(open('progress.yaml')):
+        key = i.keys()[0]
+        done = i[key].get("done",False)
+        if not done and i[key].has_key('title'):
+            print "%s: %s" % (key,i[key]['title'])
+
+if __name__ == "__main__":
+    main()
