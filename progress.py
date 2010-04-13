e4be678bd75d1aafe5ee5f51c8803a9b210f0ebd
Artem Dudarev <dudarev@locograph.(none)>
2010-04-13 12:30:13 +0300

    printing steps that are done when command 'clean' is specified

--- progress.py
+++ progress.py
@@ -10,6 +10,15 @@ def main():
         command = args[1]
         print command
 
+    if command == 'clean':
+        for i in yaml.load_all(open('progress.yaml')):
+            key = i.keys()[0]
+            done = i[key].get("done",False)
+            if done and i[key].has_key('title'):
+                print "%s: %s" % (key,i[key]['title'])
+            
+        return
+
     for i in yaml.load_all(open('progress.yaml')):
         key = i.keys()[0]
         done = i[key].get("done",False)
