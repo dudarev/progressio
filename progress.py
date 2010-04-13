18e5325068815087466439bb183bd861d0494a7e
Artem Dudarev <dudarev@locograph.(none)>
2010-04-13 13:12:44 +0300

    done tasks and steps are moved with 'clean' command

--- progress.py
+++ progress.py
@@ -11,12 +11,26 @@ def main():
         print command
 
     if command == 'clean':
+        done_list = []
+        not_done_list = []
         for i in yaml.load_all(open('progress.yaml')):
             key = i.keys()[0]
             done = i[key].get("done",False)
             if done and i[key].has_key('title'):
                 print "%s: %s" % (key,i[key]['title'])
-            
+                done_list.append(i)
+            else:
+                not_done_list.append(i)
+        stream = open('progress.history.yaml','a')
+        dump_options = {'indent':4,'default_flow_style':False, 'explicit_start':'---'}
+        for i in done_list:
+            yaml.dump(i,stream,**dump_options)
+        stream.close()
+        stream = open('progress.yaml','w')
+        dump_options = {'indent':4,'default_flow_style':False, 'explicit_start':'---'}
+        for i in not_done_list:
+            yaml.dump(i,stream,**dump_options)
+        stream.close()
         return
 
     for i in yaml.load_all(open('progress.yaml')):
