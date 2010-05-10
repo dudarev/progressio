adc3abf16669b1491eeb47993bf2ae1126e03203
Artem Dudarev <dudarev@locograph.(none)>
2010-05-10 08:54:53 +0300

    moved clean into separate function

--- progress.py
+++ progress.py
@@ -32,6 +32,29 @@ def add():
     save_items(items_list)
     return
 
+def clean():
+    done_list = []
+    not_done_list = []
+    for i in yaml.load_all(open('progress.yaml')):
+        key = i.keys()[0]
+        is_done = i[key].get("done",False)
+        if is_done and i[key].has_key('title'):
+            print "%s: %s" % (key,i[key]['title'])
+            done_list.append(i)
+        else:
+            not_done_list.append(i)
+    stream = open('progress.history.yaml','a')
+    dump_options = {'indent':4,'default_flow_style':False, 'explicit_start':'---'}
+    for i in done_list:
+        yaml.dump(i,stream,**dump_options)
+    stream.close()
+    stream = open('progress.yaml','w')
+    dump_options = {'indent':4,'default_flow_style':False, 'explicit_start':'---'}
+    for i in not_done_list:
+        yaml.dump(i,stream,**dump_options)
+    stream.close()
+    return
+
 def done():
     "mark an item done"
     try:
@@ -97,26 +120,7 @@ def main():
         print command
 
     if command == 'clean':
-        done_list = []
-        not_done_list = []
-        for i in yaml.load_all(open('progress.yaml')):
-            key = i.keys()[0]
-            is_done = i[key].get("done",False)
-            if is_done and i[key].has_key('title'):
-                print "%s: %s" % (key,i[key]['title'])
-                done_list.append(i)
-            else:
-                not_done_list.append(i)
-        stream = open('progress.history.yaml','a')
-        dump_options = {'indent':4,'default_flow_style':False, 'explicit_start':'---'}
-        for i in done_list:
-            yaml.dump(i,stream,**dump_options)
-        stream.close()
-        stream = open('progress.yaml','w')
-        dump_options = {'indent':4,'default_flow_style':False, 'explicit_start':'---'}
-        for i in not_done_list:
-            yaml.dump(i,stream,**dump_options)
-        stream.close()
+        clean()
         return
 
     if command == "html":
