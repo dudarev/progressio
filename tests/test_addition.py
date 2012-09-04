9897a0b2d190fece52e933a6b454106c2064671d
Artem Dudarev <dudarev@gmail.com>
2012-09-04 13:14:48 +0300

    c: testing addition

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,10 +1,27 @@
 import unittest
+import yaml
+
+import sys
+sys.path.insert(0,"..")
+
+from progress.progress import add
+
+def load_items():
+    return [i for i in yaml.load_all(open('progress.yaml'))]
 
 
 class TestAddition(unittest.TestCase):
 
     def test_addition(self):
-        pass
+        add('test')
+        items = load_items()
+        is_added = False
+        for i in items:
+            print i
+            if i['step']['title'] == 'test':
+                is_added = True
+                break
+        self.assertTrue(is_added)
 
 
 if __name__ == '__main__':
