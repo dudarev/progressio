0a97be42035ca593da99d9326c7ba19f83dd34d9
Artem Dudarev <dudarev@gmail.com>
2014-05-08 13:17:00 +0300

    removed unused tests

--- tests/unit/test_addition.py
+++ tests/unit/test_addition.py
@@ -10,8 +10,8 @@ from .base import BaseUnitCase
 sys.path.insert(0, "../..")
 
 from progressio.progressio import (
-    _create_dir_if_needed, _get_filename,
-    add, load_items_list, get_item, PROGRESSIO_DIR)
+    _create_dir_if_needed,
+    PROGRESSIO_DIR)
 
 TEST_DATETIME = datetime(2014, 3, 20, 12, 0, 0)
 TEST_TIMESTAMP = '20140320120000'
@@ -40,70 +40,35 @@ class TestAddition(BaseUnitCase):
         _create_dir_if_needed()
         self.assertTrue(os.path.exists(PROGRESSIO_DIR))
 
-    def test_generate_filename(self):
-        filename = _get_filename('Test  title')
-        self.assertEqual(filename, '1-test-title')
-        filename = _get_filename(u'Test title 1234 абв')
-        self.assertEqual(filename, '1-test-title-1234')
-        filename = _get_filename(u'   ')
-        self.assertEqual(filename, '1-')
+    # def test_count(self):
+    #     """Test adding two items"""
+    #     add('test1')
+    #     add('test2')
+    #     items = load_items_list()
+    #     self.assertEqual(len(items), 2)
 
-    def test_generate_filename_if_id_exists(self):
-        filename = os.path.join(PROGRESSIO_DIR, '1-')
-        with open(filename, 'w') as f:
-            f.write('Some test title')
-        # count is incremented
-        filename = _get_filename('Test  title')
-        self.assertEqual(filename, '2-test-title')
+    # def test_path_id(self):
+    #     add('test1')
+    #     item = load_items_list()[0]
+    #     self.assertEqual(item.path, '1')
+    #     add('test2')
+    #     item = load_items_list()[1]
+    #     self.assertEqual(item.path, '2')
 
-    def test_count(self):
-        """Test adding two items"""
-        add('test1')
-        add('test2')
-        items = load_items_list()
-        self.assertEqual(len(items), 2)
+    # def test_content(self):
+    #     title = 'test title'
+    #     add(title)
+    #     items = load_items_list()
+    #     self.assertEqual(items[0].title, title)
+    #     pass
 
-    def test_path_id(self):
-        add('test1')
-        item = load_items_list()[0]
-        self.assertEqual(item.path, '1')
-        add('test2')
-        item = load_items_list()[1]
-        self.assertEqual(item.path, '2')
+    # def test_add_subitem(self):
+    #     """Test that subitem may be added to some item."""
+    #     item_title = 'test1'
+    #     subitem_title = 'test11'
 
-    def test_content(self):
-        title = 'test title'
-        add(title)
-        items = load_items_list()
-        self.assertEqual(items[0].title, title)
-        pass
+    #     add(item_title)
+    #     items = load_items_list()
+    #     add(subitem_title, parent_path=items[0].path)
 
-    def test_add_subitem(self):
-        """Test that subitem may be added to some item."""
-        item_title = 'test1'
-        subitem_title = 'test11'
-
-        add(item_title)
-        items = load_items_list()
-        add(subitem_title, parent_path=items[0].path)
-
-        self.assertEqual(get_item('1/1').title, subitem_title)
-
-    # TODO: rewrite
-    def test_add_subsubitem(self):
-        """
-        Subitem to subitem can be added.
-        """
-        TEST_TEXT_SUBITEM = 'sub test'
-        TEST_TEXT_SUBSUBITEM = 'sub sub test'
-        add(TEST_TEXT_SUBITEM)
-        add('test2')
-        items = load_items_list()
-        parent_pk = items[1].pk
-        add(TEST_TEXT_SUBITEM, parent_pk=parent_pk)
-        parent = get_item(parent_pk)
-        subitem = get_item(parent.children[0])
-        add(TEST_TEXT_SUBSUBITEM, parent_pk=subitem.pk)
-        subitem = get_item(subitem.pk)
-        subsubitem_pk = subitem.children[0]
-        self.assertEqual(get_item(subsubitem_pk).title, TEST_TEXT_SUBSUBITEM)
+    #     self.assertEqual(get_item('1/1').title, subitem_title)
