cf5b3c9277fed2676cd57af4052e831ce7266c36
Artem Dudarev <dudarev@gmail.com>
2018-06-29 22:16:23 +0300

    Test listing all items

---
+++ tests/functional/test_list.py
@@ -0,0 +1,40 @@
+import unittest
+
+import os
+import sys
+import subprocess
+
+sys.path.insert(0, "..")
+
+
+SAMPLE_TASKS = """task 1
+    subtask 1-1
+task 2
+    subtask 2-1
+        subtask 2-1-1
+    subtask 2-2
+"""
+
+
+class TestList(unittest.TestCase):
+    def setUp(self):
+        """Clean up old progress files."""
+        filelist = [f for f in os.listdir(".") if f.startswith("progress.")]
+        filelist.extend([f for f in os.listdir(".") if f.startswith("TASKS.")])
+        for f in filelist:
+            os.remove(f)
+
+    def test_list_all(self):
+        """ Test listing all items """
+        with open('TASKS.md', 'w') as f:
+            f.write(SAMPLE_TASKS)
+        output = subprocess.check_output(
+            '../../progressio/progressio.py',
+            stderr=subprocess.STDOUT,
+            shell=True)
+        print 'output=', output
+        self.assertEquals(output, SAMPLE_TASKS)
+
+
+if __name__ == '__main__':
+    unittest.main()
